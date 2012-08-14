2c52123d059d36e8d78259ac3eea71d294d4938c
Simon Cropp <simon.cropp@gmail.com>
2012-08-14 20:24:17 +1000

    bump verison

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Caseless")]
 [assembly: AssemblyProduct("Caseless")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
