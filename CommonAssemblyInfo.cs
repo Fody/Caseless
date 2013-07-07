8f26d978dadbd68cc99e238763fdc2f78b18839a
Simon Cropp <simon.cropp@gmail.com>
2013-07-07 19:56:08 +1000

    Fix for TFS readonly weavers.xml

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Caseless")]
 [assembly: AssemblyProduct("Caseless")]
-[assembly: AssemblyVersion("1.2.1.1")]
-[assembly: AssemblyFileVersion("1.2.1.1")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
