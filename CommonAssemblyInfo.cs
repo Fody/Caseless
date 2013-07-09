4ada568650d372a90caed5e3a4343babcc3fad01
Simon Cropp <simon.cropp@gmail.com>
2013-07-09 22:19:17 +1000

    fix VS Guid

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Caseless")]
 [assembly: AssemblyProduct("Caseless")]
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
