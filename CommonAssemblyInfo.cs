41bc07eeaa87d9d8bab0112a1dff815a7db211ae
Simon Cropp <simon.cropp@gmail.com>
2015-08-08 17:03:52 +1000

    add missing file

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Caseless")]
 [assembly: AssemblyProduct("Caseless")]
-[assembly: AssemblyVersion("1.3.5")]
-[assembly: AssemblyFileVersion("1.3.5")]
+[assembly: AssemblyVersion("1.3.6")]
+[assembly: AssemblyFileVersion("1.3.6")]
