45e1b6043c2ea8f7d457a3079e6558892fdd2254
Simon Cropp <simon.cropp@gmail.com>
2015-08-15 13:42:57 +1000

    fix nuspec name

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Caseless")]
 [assembly: AssemblyProduct("Caseless")]
-[assembly: AssemblyVersion("1.3.6")]
-[assembly: AssemblyFileVersion("1.3.6")]
+[assembly: AssemblyVersion("1.3.7")]
+[assembly: AssemblyFileVersion("1.3.7")]
