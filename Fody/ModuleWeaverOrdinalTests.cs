d16ec24968d08fb1d9787bbc47791d5e4107f8bd
Licshee <licshee@yen.ai>
2016-02-05 14:37:10 +0800

    tests for <Caseless StringComparison="Ordinal"/>

---
+++ Fody/ModuleWeaverOrdinalTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+using Mono.Cecil;
+using NUnit.Framework;
+
+[TestFixture]
+public class ModuleWeaverOrdinalTests
+{
+    dynamic targetClass;
+    string afterAssemblyPath;
+    string beforeAssemblyPath;
+
+    public ModuleWeaverOrdinalTests()
+    {
+        beforeAssemblyPath = Path.GetFullPath(@"..\..\..\AssemblyToProcess\bin\Debug\AssemblyToProcess.dll");
+#if (!DEBUG)
+       beforeAssemblyPath = beforeAssemblyPath.Replace("Debug", "Release");
+#endif
+        afterAssemblyPath = beforeAssemblyPath.Replace(".dll", "3.dll");
+        File.Copy(beforeAssemblyPath, afterAssemblyPath, true);
+
+        var moduleDefinition = ModuleDefinition.ReadModule(afterAssemblyPath);
+
+        var weavingTask = new ModuleWeaver
+            {
+                Config = XElement.Parse(@"<Caseless StringComparison=""Ordinal""/>"),
+                ModuleDefinition = moduleDefinition,
+            };
+
+        weavingTask.Execute();
+        moduleDefinition.Assembly.Name.Name += "ForOrdinal";
+        moduleDefinition.Write(afterAssemblyPath);
+        var assembly = Assembly.LoadFrom(afterAssemblyPath);
+        var type = assembly.GetType("TargetClass", true);
+        targetClass = Activator.CreateInstance(type);
+    }
+
+    [Test]
+    public void OpEquals()
+    {
+        Assert.IsFalse(targetClass.OpEquals());
+    }
+
+    [Test]
+    public void OpEqualsWithNull()
+    {
+        Assert.IsFalse(targetClass.OpEqualsWithNull());
+    }
+
+    [Test]
+    public void OpNotEquals()
+    {
+        Assert.IsTrue(targetClass.OpNotEquals());
+    }
+
+    [Test]
+    public void OpNotEqualsWithNull()
+    {
+        Assert.IsTrue(targetClass.OpNotEqualsWithNull());
+    }
+
+    [Test]
+    public void Equals()
+    {
+        Assert.IsFalse(targetClass.Equals());
+    }
+
+    [Test]
+    public void EqualsStatic()
+    {
+        Assert.IsFalse(targetClass.EqualsStatic());
+    }
+
+    [Test]
+    public void EqualsStaticWithNull()
+    {
+        Assert.IsFalse(targetClass.EqualsStaticWithNull());
+    }
+
+
+#if(DEBUG)
+    [Test]
+    public void PeVerify()
+    {
+        Verifier.Verify(beforeAssemblyPath, afterAssemblyPath);
+    }
+#endif
+
+}
