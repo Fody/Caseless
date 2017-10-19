d51b29b77abf7725b36909a8a38bed215ce5b22d
Simon Cropp <simon.cropp@gmail.com>
2017-10-20 09:58:40 +1100

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Caseless")]
 [assembly: AssemblyProduct("Caseless")]
-[assembly: AssemblyVersion("1.7.0")]
-[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: AssemblyVersion("1.7.1")]
+[assembly: AssemblyFileVersion("1.7.1")]
